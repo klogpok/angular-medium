9d13d0fb5c09c10acfa54e263c83c4a747f90d31
Oleg Kleiman <kloleg@gmail.com>
2023-06-21 17:09:30 +0300

    Lazy start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'settings', loadChildren: () => import('./settings/settings.module').then((m) => m.SettingsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -7,7 +7,7 @@ import { settingsReducer } from './store/reducers';
 import { BackendErrorMessagesModule } from '@shared/modules/backendErrorMessages/backendErrorMessages.module';
 import { ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [{ path: 'settings', component: SettingsComponent }];
+const routes: Routes = [{ path: '', component: SettingsComponent }];
 
 @NgModule({
   declarations: [SettingsComponent],
